360f1f063b518b4dba77f7ab7bf2275e69be5b99
Shlapakova <shlapakova.kristina@mail.ru>
2024-08-18 20:24:56 +0200

    unit: upload project

--- src/index.js
+++ src/index.js
@@ -35,12 +35,46 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' '
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result = '';
+    const exprString = expr.toString();
 
+    for (let i = 0; i < exprString.length; i += 10) {
+        let dots = '';
+        const symbol = exprString.slice(i, i + 10);
+
+        if (symbol === '**********') {
+            result += ' ';
+            continue;
+        }
+
+        for (let j = 8; j >= 0; j -= 2) {
+            const pair = symbol.slice(j, j + 2);
+            switch (pair) {
+                case '10':
+                    dots = '.' + dots;
+                    break;
+                case '11':
+                    dots = '-' + dots;
+                    break;
+                case '00':
+                    break;
+            }
+        }
+
+        for (let key in MORSE_TABLE) {
+            if (dots === key) {
+                result += MORSE_TABLE[key];
+                break;
+            }
+        }
+    }
+
+    return result;
+}
 module.exports = {
     decode
 }
